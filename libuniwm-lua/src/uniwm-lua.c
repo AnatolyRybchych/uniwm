0573d58a948e51c56e7fe546b9ae7452af8a27bc
AnatolyRybchych <tol.ryb@gmail.com>
2026-06-09 21:05:13 +0000

    libuniwm-lua: supress_key/unsupress_key + register/unregister keybind
    top-level, always present (input lives in libuniwm); keybind ctx allocated
    via mc_alloc(wm_allocator); keybind_cb pcalls the lua fn

--- libuniwm-lua/src/uniwm-lua.c
+++ libuniwm-lua/src/uniwm-lua.c
@@ -1,12 +1,19 @@
 #include <uniwm-lua/uniwm-lua.h>
 
+#include <stdio.h>
+
 #include <lua.h>
 #include <lauxlib.h>
 
 #include <mc/data/str.h>
+#include <mc/data/alloc.h>
 
 #include <uniwm/wm.h>
-#include <uniwm/target.h>
+
+typedef struct Keybind {
+    lua_State *L;
+    int ref;
+} Keybind;
 
 static WM *current_wm(lua_State *L) {
     WM *wm = wm_process();
@@ -105,6 +112,71 @@ static int l_unsupress_key(lua_State *L) {
     return 0;
 }
 
+static void keybind_free(Keybind *k) {
+    luaL_unref(k->L, LUA_REGISTRYINDEX, k->ref);
+    mc_free(wm_allocator, k);
+}
+
+static void keybind_cb(void *ctx) {
+    Keybind *k = ctx;
+
+    lua_rawgeti(k->L, LUA_REGISTRYINDEX, k->ref);
+    if (lua_pcall(k->L, 0, 0, 0) != LUA_OK) {
+        fprintf(stderr, "uniwm: keybind callback error: %s\n", lua_tostring(k->L, -1));
+        lua_pop(k->L, 1);
+    }
+}
+
+static int l_register_keybind(lua_State *L) {
+    const char *spec = luaL_checkstring(L, 1);
+    luaL_checktype(L, 2, LUA_TFUNCTION);
+
+    WM_KeyCombo combo;
+    if (wm_key_combo_from_str(spec, &combo) != WM_ERROR_OK) {
+        return luaL_error(L, "uniwm.register_keybind: invalid key spec \"%s\"", spec);
+    }
+
+    WM *wm = current_wm(L);
+
+    void *old = NULL;
+    wm_unbind_key(wm, &combo, &old);
+    if (old) {
+        keybind_free(old);
+    }
+
+    Keybind *k = NULL;
+    if (mc_alloc(wm_allocator, sizeof(*k), (void **)&k) != MCE_OK) {
+        return luaL_error(L, "uniwm.register_keybind: out of memory");
+    }
+    k->L = L;
+    lua_pushvalue(L, 2);
+    k->ref = luaL_ref(L, LUA_REGISTRYINDEX);
+
+    if (wm_bind_key(wm, &combo, keybind_cb, k) != WM_ERROR_OK) {
+        keybind_free(k);
+        return luaL_error(L, "uniwm.register_keybind: failed");
+    }
+
+    return 0;
+}
+
+static int l_unregister_keybind(lua_State *L) {
+    const char *spec = luaL_checkstring(L, 1);
+
+    WM_KeyCombo combo;
+    if (wm_key_combo_from_str(spec, &combo) != WM_ERROR_OK) {
+        return luaL_error(L, "uniwm.unregister_keybind: invalid key spec \"%s\"", spec);
+    }
+
+    void *old = NULL;
+    wm_unbind_key(current_wm(L), &combo, &old);
+    if (old) {
+        keybind_free(old);
+    }
+
+    return 0;
+}
+
 static int luaopen_libuniwm(lua_State *L) {
     static const luaL_Reg vdesk_fns[] = {
         { "list", l_vdesk_list },
@@ -117,15 +189,14 @@ static int luaopen_libuniwm(lua_State *L) {
     luaL_newlib(L, vdesk_fns);
     lua_setfield(L, -2, "virtual_desktop");
 
-    const WM_TargetInterface *ti = wm_default_target();
-    if (ti && ti->suppress_key) {
-        lua_pushcfunction(L, l_supress_key);
-        lua_setfield(L, -2, "supress_key");
-    }
-    if (ti && ti->unsuppress_key) {
-        lua_pushcfunction(L, l_unsupress_key);
-        lua_setfield(L, -2, "unsupress_key");
-    }
+    lua_pushcfunction(L, l_supress_key);
+    lua_setfield(L, -2, "supress_key");
+    lua_pushcfunction(L, l_unsupress_key);
+    lua_setfield(L, -2, "unsupress_key");
+    lua_pushcfunction(L, l_register_keybind);
+    lua_setfield(L, -2, "register_keybind");
+    lua_pushcfunction(L, l_unregister_keybind);
+    lua_setfield(L, -2, "unregister_keybind");
 
     return 1;
 }
